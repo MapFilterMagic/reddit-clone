a4c265ab21782e2353dcc98ff012c7cf109d9024
mapfiltermagic <ajmurill@ucsd.edu>
2022-04-16 14:37:58 -0700

    Fixed broken config

--- spring-reddit-clone/src/main/java/com/mapfiltermagic/springredditclone/config/SecurityConfig.java
+++ spring-reddit-clone/src/main/java/com/mapfiltermagic/springredditclone/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
+        http.cors().and().csrf().disable().oauth2ResourceServer().jwt();
     }
 
     @Bean
